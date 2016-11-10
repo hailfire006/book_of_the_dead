4cd690f892df8fbfc08d626825a87d502100394c
hailfire006 <abusch@umd.edu>
2016-11-10 17:20:20 -0500

    Add files via upload

---
+++ Player_Script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Script : MonoBehaviour {
+
+
+    public Collider Coll;
+    public GameObject Laser;
+    void Start () {
+
+        Coll = Laser.GetComponent<Collider>();
+	}
+	
+	
+	void Update () {
+        
+        if (SteamVR_Controller.Input(4).GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Laser.SetActive(true);
+        }
+        if (SteamVR_Controller.Input(4).GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Laser.GetComponent<Laser_Script>().ready_to_tp = true;
+        }
+    }
+}
